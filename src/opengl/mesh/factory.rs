31c2292f1468b26fb6d49188ba0920de5fd514ba
jarvarvarvis <jarvistrigo@gmail.com>
2023-12-08 14:34:14 +0100

    Adjust mesh factory code for drawing textures

--- src/opengl/mesh/factory.rs
+++ src/opengl/mesh/factory.rs
@@ -8,28 +8,6 @@ use crate::opengl::material::Material;
 
 pub fn create_basic_quad_mesh(material: Rc<dyn Material>, tex_scale: f32) -> Mesh {
     MeshBuilder::new()
-        // Position
-        .add_vbo(|vbo| {
-            vbo.copy_data_static::<Vector3<f32>>(&[
-                Vector3::new(-1.0, 0.0, 1.0),
-                Vector3::new(1.0, 0.0, 1.0),
-                Vector3::new(1.0, 0.0, -1.0),
-                Vector3::new(-1.0, 0.0, -1.0),
-            ]);
-            vbo.set_basic_typed_vertex_attrib_pointer::<f32>(0, 3, gl::FLOAT, false);
-            vbo.set_vertex_attrib_enabled(0, true);
-        })
-        // Normal
-        .add_vbo(|vbo| {
-            vbo.copy_data_static::<Vector3<f32>>(&[
-                Vector3::y(),
-                Vector3::y(),
-                Vector3::y(),
-                Vector3::y(),
-            ]);
-            vbo.set_basic_typed_vertex_attrib_pointer::<f32>(1, 3, gl::FLOAT, false);
-            vbo.set_vertex_attrib_enabled(1, true);
-        })
         // Texture Coordinates
         .add_vbo(|vbo| {
             vbo.copy_data_static::<Vector2<f32>>(&[
@@ -38,8 +16,8 @@ pub fn create_basic_quad_mesh(material: Rc<dyn Material>, tex_scale: f32) -> Mes
                 Vector2::new(tex_scale, 0.0),
                 Vector2::new(0.0, 0.0),
             ]);
-            vbo.set_basic_typed_vertex_attrib_pointer::<f32>(2, 2, gl::FLOAT, false);
-            vbo.set_vertex_attrib_enabled(2, true);
+            vbo.set_basic_typed_vertex_attrib_pointer::<f32>(0, 2, gl::FLOAT, false);
+            vbo.set_vertex_attrib_enabled(0, true);
         })
         .set_primitive_mode(gl::TRIANGLES)
         .set_indices(&[0, 1, 2, 0, 2, 3])
