e4b22c4db43c5c51b6db14aba56ec66230a973ac
jarvarvarvis <jarvistrigo@gmail.com>
2023-12-08 14:09:51 +0100

    Add file dropping

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate glfw;
+
+use glfw::fail_on_errors;
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(512, 512, "imdrip", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.set_drag_and_drop_polling(true);
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::FileDrop(paths) => {
+                    println!("Dropped file paths:");
+                    for path in paths.iter() {
+                        println!("{}", path.display());
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
 }
